b71bedc7d0c90e4f0647eaac93c6531959f5d108
Matthew Danish <mrd45@cam.ac.uk>
2016-03-01 16:46:29 +0000

    return a NameMap and support unrename

--- src/Forpar/Analysis/Renaming.hs
+++ src/Forpar/Analysis/Renaming.hs
@@ -6,11 +6,14 @@ import Forpar.Analysis.Types
 
 import Prelude hiding (lookup)
 import Data.Maybe (maybe, fromMaybe)
-import Data.Map (findWithDefault, insert, empty, lookup, Map, fromList)
+import Data.Map (findWithDefault, insert, union, empty, lookup, Map, fromList)
 import Control.Monad.State.Lazy
 import Data.Generics.Uniplate.Data
 import Data.Generics.Uniplate.Operations
 import Data.Data
+import Data.Tuple
+
+import Text.PrettyPrint.GenericPretty
 
 --------------------------------------------------
 
@@ -34,9 +37,11 @@ declVariables f = [ v | v@DeclVariable {} <- universeBi f ]
 --------------------------------------------------
 
 type Renamer a = State RenameState a
+type NameMap = Map String String
 data RenameState = RenameState { scopeStack :: [String]
                                , uniqNums :: [Int]
-                               , environ :: [Map String String] }
+                               , environ :: [Map String String]
+                               , nameMap :: NameMap }
   deriving (Show, Eq)
 
 getUniqNum = do
@@ -44,15 +49,18 @@ getUniqNum = do
   modify $ \ s -> s { uniqNums = drop 1 (uniqNums s) }
   return uniqNum
 
-renameState0 = RenameState { scopeStack = ["_"], uniqNums = [1..], environ = [fromList []] }
+renameState0 = RenameState { scopeStack = ["_"]
+                           , uniqNums = [1..]
+                           , environ = [empty]
+                           , nameMap = empty }
 runRenamer m = runState m
 
 type RenamerFunc t = t -> Renamer t
 
 --------------------------------------------------
 
-rename :: Data a => ProgramFile a -> (ProgramFile a, RenameState)
-rename pf = (pf', s' { uniqNums = [] })
+rename :: Data a => ProgramFile a -> (ProgramFile a, NameMap)
+rename pf = (pf', nameMap s')
   where (pf', s') = runRenamer (transPU programUnit pf) renameState0
 
 programUnit :: Data a => RenamerFunc (ProgramUnit a)
@@ -60,24 +68,26 @@ programUnit pu = do
   uniqNum <- getUniqNum
   scope <- gets (head . scopeStack)
 
-  -- uniquely name the scope
+  -- uniquely name the new program unit scope
   let name = scope ++ "_" ++ mungeName (getName pu) ++ show uniqNum
-  -- push scope onto stack
+  -- push the new scope onto stack
   modify $ \ s -> s { scopeStack = name:scopeStack s }
 
-  -- if there are vars, find them
+  -- if there are parameters, find them
   let vars = case pu of PUFunction _ _ _ _ vs _ -> aStrip vs
                         PUSubroutine _ _ _ vs _ -> aStrip vs
                         _ -> []
-  -- if there are vars, create a renaming environment for them
+  -- if there are parameters, create a renaming environment for them
   pu' <- if null vars then return pu else do
     env <- flip mapM vars $ \ (ValVariable v) -> do
              uniqNum <- getUniqNum
              return (v, name ++ "_" ++ v ++ show uniqNum)
     -- also put function name, if applicable, on the renaming environment
-    let fenv = case pu of PUFunction _ _ _ n _ _ -> [(n, name)]; _ -> []
+    let fenv = (case pu of PUFunction _ _ _ n _ _ -> [(n, name)]; _ -> []) ++ env
     -- push the renaming environment on the stack and rename the vars
-    modify $ \ s -> s { environ = fromList (fenv ++ env):environ s }
+    -- also keep track of the renames in the nameMap
+    modify $ \ s -> s { environ = fromList fenv:environ s
+                      , nameMap = fromList (map swap fenv) `union` nameMap s }
     pu' <- transV_PU value pu
     -- pop and go back to where we were
     modify $ \ s -> s { environ = drop 1 (environ s) }
@@ -107,7 +117,9 @@ blstmtList st@(BlStatement _ _ _ (StDeclaration a s ty valist):_) = do
     uniqNum <- getUniqNum
     return (v, scope ++ "_" ++ v ++ show uniqNum)
   -- push the renaming environment on the stack and rename the vars
-  modify $ \ s -> s { environ = fromList env:environ s }
+  -- also keep track of the renames in the nameMap
+  modify $ \ s -> s { environ = fromList env:environ s
+                    , nameMap = fromList (map swap env) `union` nameMap s }
   st' <- transV_BS value st
   -- pop the renaming environment
   modify $ \ s -> s { environ = drop 1 (environ s) }
@@ -123,11 +135,28 @@ value v = return v
 
 --------------------------------------------------
 
+unrename :: Data a => (ProgramFile a, NameMap) -> ProgramFile a
+unrename (pf, nm) = trPU fPU (trV fV pf)
+  where
+    trV :: Data a => (Value a -> Value a) -> ProgramFile a -> ProgramFile a
+    trV = transformBi
+    fV :: Data a => Value a -> Value a
+    fV (ValVariable v) = ValVariable $ fromMaybe v (v `lookup` nm)
+    fV x               = x
+
+    trPU :: Data a => (ProgramUnit a -> ProgramUnit a) -> ProgramFile a -> ProgramFile a
+    trPU = transformBi
+    fPU :: Data a => ProgramUnit a -> ProgramUnit a
+    fPU (PUFunction a s ty n args b) = PUFunction a s ty (fromMaybe n (n `lookup` nm)) args b
+    fPU x               = x
+
+--------------------------------------------------
+
 mungeName (Named s) = s
 mungeName n = show n
 
 transPU :: Data a => RenamerFunc (ProgramUnit a) -> RenamerFunc (ProgramFile a)
-transPU = descendBiM
+transPU = transformBiM
 
 transSS :: Data a => RenamerFunc [Statement a] -> RenamerFunc (ProgramUnit a)
 transSS = descendBiM
@@ -140,6 +169,9 @@ transPU_B = descendBiM
 
 transBS_PU :: Data a => RenamerFunc [Block a] -> RenamerFunc (ProgramUnit a)
 transBS_PU = transformBiM
+
+transV_PF :: Data a => RenamerFunc (Value a) -> RenamerFunc (ProgramFile a)
+transV_PF = transformBiM
 
 transV_PU :: Data a => RenamerFunc (Value a) -> RenamerFunc (ProgramUnit a)
 transV_PU = descendBiM
