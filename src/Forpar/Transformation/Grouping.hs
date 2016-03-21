612e6e4d2847b73bfbac5558389fbf4fa38d0317
madgen <me@madgen.net>
2016-03-21 01:48:13 +0000

    Refactor and comment IF grouping

--- src/Forpar/Transformation/Grouping.hs
+++ src/Forpar/Transformation/Grouping.hs
@@ -6,47 +6,89 @@ import Forpar.Transformation.TransformMonad
 groupIf :: Transform ()
 groupIf = do
     ProgramFile pus e <- getProgramFile
-    putProgramFile $ ProgramFile (zip (map fst pus) . map aux . map snd $ pus) e
+    putProgramFile $ ProgramFile (zip (map fst pus) . map (go . snd) $ pus) e
   where
-    aux pu =
+    go pu =
       case pu of
         PUMain a s n bs -> PUMain a s n $ groupIf' bs
         PUSubroutine a s n as bs -> PUSubroutine a s n as $ groupIf' bs
         PUFunction a s r n as bs -> PUFunction a s r n as $ groupIf' bs
-        bd@(PUBlockData _ _ _ _) -> bd
+        bd@PUBlockData{} -> bd -- Block data cannot have any if statements.
 
+-- Actual grouping is done here.
+-- 1. Case: head is a statement block with an IF statement:
+-- 1.1  Group everything to the right of the statement.
+-- 1.2  Prepend the head 
+-- 1.3  Decompose into if components (blocks and condition pairs).
+-- 1.4  Using original if statement and decomposition artefacts synthesise a
+--        structured if block.
+-- 1.5  Prepend the block to the left over artefacts, which have already been
+--        grouped in 1.1
+-- 2. Case: head is a statement block contianing any other statement:
+-- 2.1  Group everything to the right and prepend the head.
 groupIf' :: [ Block a ] -> [ Block a ]
 groupIf' [] = []
-groupIf' bs@((BlStatement _ _ _ (StIfThen _ _ _)):_) = 
-  let (bif, bs') = extractIfBlock bs in bif : groupIf' bs'
-groupIf' (b:bs) = b : groupIf' bs
+groupIf' blocks@(b:bs)
+  | BlStatement a s label StIfThen{} <- b = -- If statement
+      let ( conditions, blocks, leftOverBlocks ) = 
+            breakIntoIfComponents (b:groupedBlocks)
+      in BlIf a (getTransSpan s blocks) label conditions blocks
+         : leftOverBlocks
+  | otherwise = b : groupedBlocks -- Not an if statement
+  where
+    groupedBlocks = groupIf' bs -- Assume everything to the right is grouped.
 
-extractIfBlock :: [ Block a ] -> (Block a, [ Block a ])
-extractIfBlock (bif@(BlStatement a _ _ (StIfThen _ _ _)):bs) = 
-  let gbs = groupIf' bs 
-      (conds, blocks, rest) = collectIfComponents (bif:gbs)
-      lastBlock = last . last $ blocks
-      span = getTransSpan bif lastBlock in
-    (BlIf a span (getLabel bif) conds blocks, rest)
-extractIfBlock _ = error "Head needs to be an IF-THEN statement"
-
-collectIfComponents :: [ Block a ] -> ([ Maybe (Expression a) ], [ [ Block a ] ], [ Block a ])
-collectIfComponents bs =
-  case bs of 
-    (BlStatement _ _ _ (StIfThen _ _ e)):rest -> aux (Just e) rest
-    (BlStatement _ _ _ (StElsif _ _ e)):rest -> aux (Just e) rest
-    (BlStatement _ _ _ (StElse _ _)):rest -> aux Nothing rest
-    (BlStatement _ _ _ (StEndif _ _)):rest -> ([], [], rest)
-    _ -> error "Mismatch IF statement"
+-- A program has the following structure:
+--
+--[ block... ]
+-- if <condition> then
+--   [ block... ]
+-- else if <condition>
+--   [ block... ]
+-- else 
+--   [ block... ]
+-- end if
+-- [ block... ]
+--
+-- This function must only receive a list of blocks that start with if.
+--
+-- Internally it uses a more permissive breaking function that processes
+-- individual (if-then, block), (else-if, block), and (else, block) pairs.
+--
+-- In that case it decomposes the block into list of (maybe) conditions and
+-- blocks that those conditions correspond to. Additionally, it returns
+-- whatever is after the if block.
+breakIntoIfComponents :: [ Block a ] -> ([ Maybe (Expression a) ], [ [ Block a ] ], [ Block a ])
+breakIntoIfComponents blocks@(BlStatement _ _ _ StIfThen{}:rest) = 
+    breakIntoIfComponents' blocks
   where 
-    aux e bs = 
-      let (bs', rest') = collectStandardBlocks bs
-          (cs, bs'', rest'') = collectIfComponents rest'
-      in (e:cs, bs':bs'', rest'')
+    breakIntoIfComponents' (BlStatement _ _ _ st:rest) = 
+      case st of
+        StIfThen _ _ condition -> go (Just condition) rest
+        StElsif _ _ condition -> go (Just condition) rest
+        StElse{} -> go Nothing rest
+        StEndif{} -> ([], [], rest)
+        _ -> error "Block with non-if related statement. Should never occur."
+    go maybeCondition blocks = 
+      let (nonConditionBlocks, rest') = collectNonConditionalBlocks blocks
+          (conditions, listOfBlocks, rest'') = breakIntoIfComponents' rest'
+      in ( maybeCondition : conditions
+         , nonConditionBlocks : listOfBlocks
+         , rest'' )
 
-collectStandardBlocks :: [ Block a ] -> ([ Block a ], [ Block a ])
-collectStandardBlocks all@((BlStatement _ _ _ (StElsif _ _ e)):_) = ([], all)
-collectStandardBlocks all@((BlStatement _ _ _ (StElse _ _)):_) = ([], all)
-collectStandardBlocks all@(b@(BlStatement _ _ _ (StEndif _ _)):_) = ([ b ], all)
-collectStandardBlocks (x:xs) = 
-  let (bs, rest) = collectStandardBlocks xs in (x : bs, rest)
+-- This compiles the executable blocks under various if conditions.
+collectNonConditionalBlocks :: [ Block a ] -> ([ Block a ], [ Block a ])
+collectNonConditionalBlocks blocks =
+  case blocks of
+    BlStatement _ _ _ StElsif{}:_ -> ([], blocks)
+    BlStatement _ _ _ StElse{}:_ -> ([], blocks)
+    -- Here end block is included within the blocks unlike the other
+    -- conditional directives. The reason is that this block can be
+    -- a branch target if it is labeled according to the specification, hence 
+    -- it is presence in the parse tree is meaningful.
+    b@(BlStatement _ _ _ StEndif{}):_ -> ([ b ], blocks)
+    -- Catch all case for all non-if related blocks.
+    b:bs -> let (bs', rest) = collectNonConditionalBlocks bs in (b : bs', rest)
+    -- In this case the structured if block is malformed and the file ends
+    -- prematurely.
+    _ -> error "Premature file ending while parsing structured if block."
