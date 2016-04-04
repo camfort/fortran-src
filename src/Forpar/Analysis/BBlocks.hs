ad39aa73e7abdaa32ef758f2c409de38b985607a
Matthew Danish <mrd45@cam.ac.uk>
2016-04-04 19:46:51 +0100

    clean-up code and handle Goto/Return statements

--- src/Forpar/Analysis/BBlocks.hs
+++ src/Forpar/Analysis/BBlocks.hs
@@ -30,8 +30,24 @@ toBBlocksPerPU pu
     bs  = case pu of PUMain _ _ _ bs -> bs; PUSubroutine _ _ _ _ bs -> bs; PUFunction _ _ _ _ _ bs -> bs
                      _ -> []
     bbs = execBBlocker (processBlocks bs)
-    gr  = insEdges (newEdges bbs) (bbGraph bbs)
+    gr  = insExitEdges lm . insEntryEdges $ insEdges (newEdges bbs) (bbGraph bbs)
     pu' = setAnnotation ((getAnnotation pu) { bBlocks = Just gr }) pu
+    lm  = labelMap bbs
+
+insEntryEdges = insEdge (0, 1, ()) . insNode (0, []) -- for now assume only one entry
+
+insExitEdges lm gr = insEdges (map doNode deadEnds) $ insNode (-1, []) gr
+  where
+    deadEnds = nodes gr >>= \ n -> if null (gr `out` n) then [n] else []
+    doNode n = (n, examineFinalBlock lm bs, ())
+      where bs = fromJust $ gr `lab` n
+
+-- find target of Goto statements (Return statements default target to -1)
+examineFinalBlock lm bs@(_:_)
+  | BlStatement _ _ _ g@(StGotoUnconditional {}) <- last bs = gotoBlock lm g
+examineFinalBlock _ _ = -1
+
+gotoBlock lm (StGotoUnconditional _ _ (ExpValue _ _ (ValLabel l))) = (-1) `fromMaybe` M.lookup l lm
 
 --------------------------------------------------
 
@@ -48,102 +64,119 @@ bbs0 = BBS { bbGraph = empty, curBB = [], curNode = 1, labelMap = M.empty, nums 
 execBBlocker :: BBlocker a b -> BBState a
 execBBlocker = flip execState bbs0
 
+-- handle a list of blocks (typically from ProgramUnit or nested inside a BlDo, BlIf, etc).
 processBlocks :: [Block a] -> BBlocker a (Node, Node)
 -- precondition: curNode is not yet in the graph && will label the first block
 -- postcondition: final bblock is in the graph labeled as endN && curNode == endN
 -- returns start and end nodes for basic block graph corresponding to parameter bs
 processBlocks bs = do
   startN <- gets curNode
-  forM_ bs perBlock
-  st@BBS { curBB = bb, bbGraph = gr, curNode = endN } <- get
-  let gr' = insNode (endN, reverse bb) gr
-  modify $ \ st -> st { curBB = [], bbGraph = gr' }
+  mapM_ perBlock bs
+  endN   <- gets curNode
+  modify $ \ st -> st { bbGraph = insNode (endN, reverse (curBB st)) (bbGraph st)
+                      , curBB   = [] }
   return (startN, endN)
 
+--------------------------------------------------
+
+-- handle an AST-block element
 perBlock :: Block a -> BBlocker a ()
 -- invariant: curNode corresponds to curBB, and is not yet in the graph
 -- invariant: curBB is in reverse order
 perBlock b@(BlIf _ _ _ exps bss) = do
   processLabel b
-  st@BBS { curBB = bb, bbGraph = gr, curNode = ifN, labelMap = lm, newEdges = es } <- get
-  -- close the previous bblock and save it, putting this If-statement at the end.
-  let gr' = insNode (ifN, reverse (b:bb)) gr
-  modify $ \ st -> st { bbGraph = gr', curBB = [] }
+  addToBBlock b
+  (ifN, _) <- closeBBlock
 
   -- go through nested AST-blocks
   startEnds <- forM bss $ \ bs -> do
-    -- start a bblock for this nested AST-block
-    thenN <- gen
-    modify $ \ st -> st { curNode = thenN, curBB = [] }
-    (_, endN) <- processBlocks bs
+    (thenN, endN) <- processBlocks bs
+    genBBlock
     return (thenN, endN)
 
   -- connect all the new bblocks with edges, link to subsequent bblock labeled nxtN
-  nxtN      <- gen
-  let es    = startEnds >>= \ (thenN, endN) -> [(ifN, thenN, ()), (endN, nxtN, ())]
+  nxtN   <- gets curNode
+  let es  = startEnds >>= \ (thenN, endN) -> [(ifN, thenN, ()), (endN, nxtN, ())]
   -- if there is no "Else"-statement then we need an edge from ifN -> nxtN
-  let es'   = if any isNothing exps then es else (ifN, nxtN, ()):es
-  modify $ \ st -> st { newEdges = es' ++ newEdges st, curNode = nxtN, curBB = [] }
+  createEdges $ if any isNothing exps then es else (ifN, nxtN, ()):es
+
 perBlock b@(BlStatement a ss _ (StIfLogical _ _ exp stm)) = do
   processLabel b
-  st@BBS { curBB = bb, bbGraph = gr, curNode = ifN, labelMap = lm, newEdges = es } <- get
-  -- close the previous bblock and save it, putting this If-statement at the end.
-  let gr' = insNode (ifN, reverse (b:bb)) gr
+  addToBBlock b
 
   -- start a bblock for the nested statement inside the If
-  thenN <- gen
-  modify $ \ st -> st { curNode = thenN, curBB = [], bbGraph = gr' }
+  (ifN, thenN) <- closeBBlock
 
   -- build pseudo-AST-block to contain nested statement
   processBlocks [BlStatement a ss Nothing stm]
+  endN <- gets curNode
 
-  st@BBS { bbGraph = gr, newEdges = es, curNode = endN } <- get
   -- connect all the new bblocks with edges, link to subsequent bblock labeled nxtN
-  nxtN <- gen
-  let es' = (ifN, thenN, ()):(ifN, nxtN, ()):(thenN, nxtN, ()):es
-  modify $ \ st -> st { curNode = nxtN, curBB = [], newEdges = es' }
+  nxtN <- genBBlock
+  createEdges [(ifN, thenN, ()), (ifN, nxtN, ()), (thenN, nxtN, ())]
+
 perBlock b@(BlStatement _ _ _ (StIfArithmetic {})) = error "BBlocks: StIfArithmetic unsupported"
 perBlock b@(BlDo _ _ mlab spec bs) = perDoBlock b bs
 perBlock b@(BlDoWhile _ _ mlab spec bs) = perDoBlock b bs
-perBlock b = do
-  processLabel b
-  bb <- gets curBB
-  let bb' = b:bb
-  modify $ \ st -> st { curBB = bb' }
+perBlock b@(BlStatement _ _ _ (StReturn {})) =
+  processLabel b >> addToBBlock b >> closeBBlock_
+perBlock b@(BlStatement _ _ _ (StGotoUnconditional {})) =
+  processLabel b >> addToBBlock b >> closeBBlock_
+perBlock b = processLabel b >> addToBBlock b
 
+--------------------------------------------------
+-- helpers
+
+-- do-block helper
 perDoBlock :: Block a -> [Block a] -> BBlocker a ()
 perDoBlock b bs = do
-  st@BBS { curBB = bb, bbGraph = gr, curNode = n, labelMap = lm, newEdges = es } <- get
-  -- close the previous bblock and save it
-  let gr' = insNode (n, reverse bb) gr
-
-  -- now, create a new bblock to hold the conditional implicit in the do-statement
-  doN <- gen
-  let lm' = case getLabel b of Just (ExpValue _ _ (ValLabel l)) -> M.insert l doN lm; _ -> lm
-  let gr'' = insNode (doN, [b]) gr'
-
+  (n, doN) <- closeBBlock
+  case getLabel b of
+    Just (ExpValue _ _ (ValLabel l)) -> insertLabel l doN
+    _                                -> return ()
+  addToBBlock b
+  closeBBlock
   -- process nested bblocks inside of do-statement
-  startN <- gen
-  modify $ \ st -> st { curNode = startN, curBB = [], bbGraph = gr'', labelMap = lm' }
-  processBlocks bs
-  st@BBS { bbGraph = gr, newEdges = es, curNode = endN } <- get
-
+  (startN, endN) <- processBlocks bs
+  (_, n') <- closeBBlock
   -- connect all the new bblocks with edges, link to subsequent bblock labeled n'
-  n' <- gen
-  let es' = (n, doN, ()):(doN, n', ()):(doN, startN, ()):(endN, doN, ()):es
-  modify $ \ st -> st { curNode = n', curBB = [], newEdges = es' }
+  createEdges [(n, doN, ()), (doN, n', ()), (doN, startN, ()), (endN, doN, ())]
 
-processLabel :: Block a -> BBlocker a ()
 -- maintains perBlock invariants while potentially starting a new bblock in case of a label
+processLabel :: Block a -> BBlocker a ()
 processLabel b | Just (ExpValue _ _ (ValLabel l)) <- getLabel b = do
-  st@BBS { curBB = bb, bbGraph = gr, curNode = n, labelMap = lm, newEdges = es } <- get
-  n' <- gen
-  let gr' = insNode (n, reverse bb) gr
-  let es' = (n, n', ()):es
-  let lm' = M.insert l n' lm
-  modify $ \ st -> st { curNode = n', curBB = [], bbGraph = gr', labelMap = lm', newEdges = es' }
+  (n, n') <- closeBBlock
+  insertLabel l n'
+  createEdges [(n, n', ())]
 processLabel _ = return ()
 
+-- inserts into labelMap
+insertLabel l n = modify $ \ st -> st { labelMap = M.insert l n (labelMap st) }
+
+-- puts an AST block into the current bblock
+addToBBlock :: Block a -> BBlocker a ()
+addToBBlock b = modify $ \ st -> st { curBB = b:curBB st }
+
+-- closes down the current bblock and opens a new one
+closeBBlock :: BBlocker a (Node, Node)
+closeBBlock = do
+  n  <- gets curNode
+  modify $ \ st -> st { bbGraph = insNode (n, reverse (curBB st)) (bbGraph st), curBB = [] }
+  n' <- genBBlock
+  return (n, n')
+closeBBlock_ = closeBBlock >> return ()
+
+-- starts up a new bblock
+genBBlock :: BBlocker a Int
+genBBlock = do
+  n' <- gen
+  modify $ \ st -> st { curNode = n', curBB = [] }
+  return n'
+
+-- adds labeled-edge mappings
+createEdges es = modify $ \ st -> st { newEdges = es ++ newEdges st }
+
+-- generates a new node number
 gen :: BBlocker a Int
 gen = do
   n:ns <- gets nums
