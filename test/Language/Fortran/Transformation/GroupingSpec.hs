706f7d85c7588ac2dd27eeb041af297ddfed1612
dominic <dom.orchard@gmail.com>
2016-08-10 15:39:47 +0100

    groupding do tests

--- test/Language/Fortran/Transformation/GroupingSpec.hs
+++ test/Language/Fortran/Transformation/GroupingSpec.hs
@@ -7,15 +7,23 @@ import Language.Fortran.Transformer
 import Language.Fortran.AST
 
 groupIf = transform [ GroupIf ]
+groupDo = transform [ GroupLabeledDo ]
 
 spec :: Spec
-spec =
+spec = do
   describe "Block IF-THEN and related statements" $ do
     it "groups example1" $
       groupIf example1 `shouldBe'` expectedExample1
 
     it "groups example2" $
       groupIf example2 `shouldBe'` expectedExample2
+
+  describe "Block DO statements" $ do
+    it "do group example1" $
+      groupDo example1do `shouldBe` expectedExample1do
+
+    it "do group example2 with common end-point" $
+      groupDo example2do `shouldBe` expectedExample2do
 
 -- if (.true.) then
 -- end if
@@ -57,3 +65,40 @@ blockGroups =
   , [ innerIf
     , BlStatement () u Nothing (StEndif () u Nothing) ] ]
 innerIf = BlIf () u Nothing [ Just valFalse ] [ [ BlStatement () u Nothing (StEndif () u Nothing) ] ]
+
+
+-- do 10 i = 0, 10
+-- 10   continue
+example1do = ProgramFile mi77 [ ([ ], PUMain () u (Just "example1") example1doblocks Nothing) ] [ ]
+example1doblocks =
+  [ BlStatement () u Nothing
+      (StDo () u Nothing (Just (ExpValue () u (ValInteger "10"))) dospec) ,
+    BlStatement () u (Just (ExpValue () u (ValInteger "10"))) (StContinue () u) ]
+dospec = (Just (DoSpecification () u
+          (StExpressionAssign () u (ExpValue () u (ValVariable "i"))
+                                   (ExpValue () u (ValInteger "0")))
+                                   (ExpValue () u (ValInteger "10")) Nothing))
+
+expectedExample1do = ProgramFile mi77 [ ([ ], PUMain () u (Just "example1") expectedExample1doBlocks Nothing) ] [ ]
+expectedExample1doBlocks =
+  [ BlDo () u Nothing dospec
+     [BlStatement () u (Just (ExpValue () u (ValInteger "10"))) (StContinue () u)]]
+
+
+-- do 10 i = 0, 10
+-- do 10 i = 0, 10
+-- 10   continue
+example2do = ProgramFile mi77 [ ([ ], PUMain () u (Just "example2") example2doblocks Nothing) ] [ ]
+example2doblocks =
+  [ BlStatement () u Nothing
+      (StDo () u Nothing (Just (ExpValue () u (ValInteger "10"))) dospec),
+    BlStatement () u Nothing
+      (StDo () u Nothing (Just (ExpValue () u (ValInteger "10"))) dospec),
+    BlStatement () u (Just (ExpValue () u (ValInteger "10"))) (StContinue () u) ]
+
+expectedExample2do = ProgramFile mi77 [ ([ ], PUMain () u (Just "example2") expectedExample2doBlocks Nothing) ] [ ]
+expectedExample2doBlocks =
+  [ BlStatement () u Nothing
+      (StDo () u Nothing (Just (ExpValue () u (ValInteger "10"))) dospec),
+    BlDo () u Nothing dospec
+     [BlStatement () u (Just (ExpValue () u (ValInteger "10"))) (StContinue () u)]]
