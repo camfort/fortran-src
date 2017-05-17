e3ff648315e991559e92cd85dd4f77e0020fe69d
Matthew Danish <mrd45@cam.ac.uk>
2017-05-17 17:40:30 +0100

    adjust formatting

--- src/Language/Fortran/Analysis.hs
+++ src/Language/Fortran/Analysis.hs
@@ -108,21 +108,21 @@ analysis0 a = Analysis { prevAnnotation = a
 
 -- | Obtain either uniqueName or source name from an ExpValue variable.
 varName :: Expression (Analysis a) -> String
-varName (ExpValue (Analysis { uniqueName = Just n }) _ (ValVariable {})) = n
-varName (ExpValue (Analysis { sourceName = Just n }) _ (ValVariable {})) = n
-varName (ExpValue _ _ (ValVariable n))                                   = n
+varName (ExpValue (Analysis { uniqueName = Just n }) _ (ValVariable {}))  = n
+varName (ExpValue (Analysis { sourceName = Just n }) _ (ValVariable {}))  = n
+varName (ExpValue _ _ (ValVariable n))                                    = n
 varName (ExpValue (Analysis { uniqueName = Just n }) _ (ValIntrinsic {})) = n
 varName (ExpValue (Analysis { sourceName = Just n }) _ (ValIntrinsic {})) = n
 varName (ExpValue _ _ (ValIntrinsic n))                                   = n
-varName _ = error "Use of varName on non-variable."
+varName _                                                                 = error "Use of varName on non-variable."
 
 -- | Obtain the source name from an ExpValue variable.
 srcName :: Expression (Analysis a) -> String
-srcName (ExpValue (Analysis { sourceName = Just n }) _ (ValVariable {})) = n
-srcName (ExpValue _ _ (ValVariable n))                                   = n
+srcName (ExpValue (Analysis { sourceName = Just n }) _ (ValVariable {}))  = n
+srcName (ExpValue _ _ (ValVariable n))                                    = n
 srcName (ExpValue (Analysis { sourceName = Just n }) _ (ValIntrinsic {})) = n
 srcName (ExpValue _ _ (ValIntrinsic n))                                   = n
-srcName _ = error "Use of srcName on non-variable."
+srcName _                                                                 = error "Use of srcName on non-variable."
 
 -- | Generate an ExpValue variable with its source name == to its uniqueName.
 genVar :: Analysis a -> SrcSpan -> String -> Expression (Analysis a)
