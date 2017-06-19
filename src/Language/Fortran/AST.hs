d1c466a887cf9b558b2e170ac25e9d56bb238ade
Harry Clarke <harry-clarke@outlook.com>
2017-06-19 13:45:23 +0100

    Added PURE/ELEMENTAL to AST.

--- src/Language/Fortran/AST.hs
+++ src/Language/Fortran/AST.hs
@@ -108,7 +108,7 @@ data ProgramUnit a =
   | PUFunction
       a SrcSpan
       (Maybe (TypeSpec a)) -- Return type
-      Bool -- Recursive or not
+      PUFunctionOpt -- Function Options
       Name
       (Maybe (AList Expression a)) -- Arguments
       (Maybe (Expression a)) -- Result
@@ -120,6 +120,8 @@ data ProgramUnit a =
       [Block a] -- Body
   | PUComment a SrcSpan (Comment a)
   deriving (Eq, Show, Data, Typeable, Generic, Functor)
+
+data PUFunctionOpt = None Bool | Pure Bool | Elemental
 
 programUnitBody :: ProgramUnit a -> [Block a]
 programUnitBody (PUMain _ _ _ bs _)              = bs
