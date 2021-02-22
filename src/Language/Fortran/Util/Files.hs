47877b6d14c667c29ed9432cf66ff98a7ae67323
Ben Orchard <thefirstmuffinman@gmail.com>
2021-02-22 14:40:32 +0000

    expose rGetDirContents as file util
    Type updated to reflect meaning (but unchanged: FilePath == String).

--- src/Language/Fortran/Util/Files.hs
+++ src/Language/Fortran/Util/Files.hs
@@ -1,13 +1,16 @@
 module Language.Fortran.Util.Files
   ( flexReadFile
   , getDirContents
+  , rGetDirContents
   ) where
 
 import qualified Data.Text.Encoding         as T
 import qualified Data.Text.Encoding.Error   as T
 import qualified Data.ByteString.Char8      as B
-import           System.Directory (listDirectory, canonicalizePath)
+import           System.Directory (listDirectory, canonicalizePath,
+                                   doesDirectoryExist, getDirectoryContents)
 import           System.FilePath  ((</>))
+import           Data.List        ((\\))
 
 -- | Obtain a UTF-8 safe 'B.ByteString' representation of a file's contents.
 --
@@ -20,3 +23,19 @@ getDirContents :: FilePath -> IO [FilePath]
 getDirContents d = do
   d' <- canonicalizePath d
   map (d' </>) `fmap` listDirectory d'
+
+-- | List files in directory recursively.
+rGetDirContents :: FilePath -> IO [FilePath]
+rGetDirContents d = canonicalizePath d >>= \d' -> go [d'] d'
+  where
+    go seen d'' = do
+      ds <- getDirectoryContents d''
+      fmap concat . mapM f $ ds \\ [".", ".."] -- remove '.' and '..' entries
+        where
+          f x = do
+            path <- canonicalizePath $ d ++ "/" ++ x
+            g <- doesDirectoryExist path
+            if g && notElem path seen then do
+              x' <- go (path : seen) path
+              return $ map (\ y -> x ++ "/" ++ y) x'
+            else return [x]
