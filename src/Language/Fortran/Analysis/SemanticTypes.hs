aafb718e752006740e861b6a19755297ca57959e
Ben Orchard <thefirstmuffinman@gmail.com>
2021-06-07 10:55:27 +0100

    migrate some kinds utils

--- src/Language/Fortran/Analysis/SemanticTypes.hs
+++ src/Language/Fortran/Analysis/SemanticTypes.hs
@@ -7,7 +7,11 @@ module Language.Fortran.Analysis.SemanticTypes where
 import           Data.Data                      ( Data, Typeable )
 import           Control.DeepSeq                ( NFData )
 import           GHC.Generics                   ( Generic )
-import           Language.Fortran.AST           ( Kind, Expression(..), Value(..), Selector(..) )
+import           Language.Fortran.AST           ( BaseType(..)
+                                                , Kind
+                                                , Expression(..)
+                                                , Value(..)
+                                                , Selector(..) )
 import           Data.Binary                    ( Binary )
 import           Text.PrettyPrint.GenericPretty ( Out(..) )
 
@@ -94,3 +98,56 @@ charLenConcat l1 l2 = case (l1, l2) of
   (_             , CharLenColon  ) -> CharLenColon
   (CharLenInt i1 , CharLenInt i2 ) -> CharLenInt (i1 + i2)
 
+--------------------------------------------------------------------------------
+
+-- | Given a 'BaseType' infer the "default" kind (or size of the
+-- variable in memory).
+--
+-- Useful when you need a default kind, but gives you an unwrapped type.
+-- Consider using Analysis.deriveSemTypeFromBaseType also.
+--
+-- Further documentation:
+-- https://docs.oracle.com/cd/E19957-01/805-4939/c400041360f5/index.html
+kindOfBaseType :: BaseType -> Int
+kindOfBaseType = \case
+  TypeInteger         -> 4
+  TypeReal            -> 4
+  TypeDoublePrecision -> 8
+  TypeComplex         -> 8
+  TypeDoubleComplex   -> 16
+  TypeLogical         -> 4
+  TypeCharacter{}     -> 1
+  TypeByte            -> 1
+
+  -- arbitrary values (>F77 is not tested/used)
+  TypeCustom{}        -> 1
+  ClassStar           -> 1
+  ClassCustom{}       -> 1
+
+getTypeSize :: SemType -> Maybe Int
+getTypeSize = \case
+  TInteger      k   -> Just k
+  TReal         k   -> Just k
+  TComplex      k   -> Just k
+  TLogical      k   -> Just k
+  TByte         k   -> Just k
+  TArray     ty _   -> getTypeSize ty
+  TCustom       _   -> Just 1
+  -- char: treat length as "kind" (but also use recorded kind)
+  TCharacter (CharLenInt l) k -> Just (l * k)
+  TCharacter _              _ -> Nothing
+
+setTypeSize :: SemType -> Maybe Int -> SemType
+setTypeSize ty mk = case (mk, ty) of
+  (Just k, TInteger _  ) -> TInteger k
+  (Just k, TReal _     ) -> TReal k
+  (Just k, TComplex _  ) -> TComplex k
+  (Just k, TLogical _  ) -> TLogical k
+  (Just k, TByte _     ) -> TByte k
+  (_     , TCustom s   ) -> TCustom s
+  -- char: treat length as "kind"
+  (Just l, TCharacter _ k) ->
+    TCharacter (CharLenInt l) k
+  (Nothing, TCharacter _ k) ->
+    TCharacter CharLenStar k
+  _ -> error $ "Tried to set invalid kind for type " <> show ty
