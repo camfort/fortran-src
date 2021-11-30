f731f362efe3d10bd69c1a4b8b20f27c0711d038
Ben Orchard <thefirstmuffinman@gmail.com>
2021-11-30 14:35:23 +0000

    add another BOZ test

--- test/Language/Fortran/AST/BozSpec.hs
+++ test/Language/Fortran/AST/BozSpec.hs
@@ -9,3 +9,6 @@ spec = do
   describe "BOZ literal constants" $ do
     it "parses a prefix and suffix BOZ constant identically" $ do
       parseBoz "z'123abc'" `shouldBe` parseBoz "'123abc'z"
+
+    it "parses nonstandard X as Z (hex)" $ do
+      parseBoz "x'09af'" `shouldBe` parseBoz "z'09af'"
