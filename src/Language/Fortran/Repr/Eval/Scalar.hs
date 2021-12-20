d3655f9bce4dfd141ce3195164f4cc7d75011960
Dominic Orchard <dom.orchard@gmail.com>
2021-12-20 17:20:17 +0000

    basic operation evaluation for integer operations

--- src/Language/Fortran/Repr/Eval/Scalar.hs
+++ src/Language/Fortran/Repr/Eval/Scalar.hs
@@ -3,6 +3,7 @@
 module Language.Fortran.Repr.Eval.Scalar where
 
 import           Language.Fortran.AST
+import           Language.Fortran.Repr.Type
 import           Language.Fortran.Repr.Value
 
 import qualified Data.Data as Data
@@ -19,16 +20,25 @@ data Error
   = ErrorVarUndefined Name
   | ErrorUnsupportedExpression String
   | ErrorUnsupportedValue String
+  | ErrorUnsupportedCommonType String String
     deriving (Eq, Show)
 
 eval :: Data a => Env -> Expression a -> Either Error FValScalar
-eval env = \case
+eval env v = case v of
   ExpValue _ _ valExpr -> case valExpr of
     ValVariable v -> do
       case Map.lookup v (envVars env) of
         Nothing  -> Left $ ErrorVarUndefined v
         Just val -> return val
     _ -> evalValue valExpr
+
+  ExpBinary a s op valExpr1 valExpr2 -> do
+    res1 <- eval env valExpr1
+    res2 <- eval env valExpr2
+    case evalBinaryOp op res1 res2 of
+      Left  _   -> Left $ ErrorUnsupportedExpression $ show $ Data.toConstr v
+      Right res -> Right res
+
   e -> Left $ ErrorUnsupportedExpression $ show $ Data.toConstr e
 
 -- | Must not be a 'ValVariable'.
@@ -39,3 +49,20 @@ evalValue = \case
 
 evalInt :: String -> Maybe (Expression a) -> Either Error FValInt
 evalInt = undefined
+
+evalBinaryOp :: BinaryOp -> FValScalar -> FValScalar -> Either Error FValScalar
+evalBinaryOp op (FValScalarInt v1@(FValInt ty1 val1)) (FValScalarInt v2@(FValInt ty2 val2)) =
+  case joinType ty1 ty2 of
+    Nothing -> Left  $ ErrorUnsupportedCommonType (show ty1) (show ty2)
+    Just jt ->
+      (FValScalarInt . FValInt jt) <$>
+        case op of
+          Addition       -> Right $ val1 + val2
+          Multiplication -> Right $ val1 * val2
+          Subtraction    -> Right $ val1 - val2
+          -- Division is not a homomorphism wrt. representation, so must convert first
+          Division       -> Right $ fvalInt (toRuntimeRepr v1) `div`  fvalInt (toRuntimeRepr v2)
+          _              -> Left $ ErrorUnsupportedExpression ""
+
+
+evalBinaryOp _ _ _ = Left $ ErrorUnsupportedValue ""
