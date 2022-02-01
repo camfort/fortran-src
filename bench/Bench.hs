82210c3825db4309d391dfdb4e3ce27aabc8ba96
Ben Orchard <thefirstmuffinman@gmail.com>
2022-02-01 12:51:52 +0000

    update

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,21 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Criterion.Main
-import           Language.Fortran.Parser.Any
-import           Language.Fortran.ParserMonad
+
+import qualified Language.Fortran.Parser        as Parser
 import           Language.Fortran.Version
 import           Language.Fortran.AST
 
-import qualified Language.Fortran.Parser.Fortran90 as F90
-import qualified Language.Fortran.Parser.Fortran77 as F77
-import qualified Language.Fortran.Parser.Fortran66 as F66
-import qualified Language.Fortran.Lexer.FreeForm   as LexFree
-import qualified Language.Fortran.Lexer.FixedForm  as LexFixed
-
 import           Data.ByteString (ByteString)
-import qualified Data.Text as Text
-import qualified Data.Text.Encoding as TSE
+import qualified Data.Text                      as Text
+import qualified Data.Text.Encoding             as Text
 import           Data.List (intercalate)
+
+import qualified Language.Fortran.Parser.Free.Fortran90 as F90
 
 main :: IO ()
 main = defaultMain
@@ -45,59 +41,15 @@ main = defaultMain
 --------------------------------------------------------------------------------
 
 pF90Stmt :: ByteString -> Statement A0
-pF90Stmt = parseFree Fortran90 F90.statementParser
+pF90Stmt = Parser.parseUnsafe $ Parser.makeParserFree F90.statementParser Fortran90
 
-pF90Func :: ByteString -> ProgramUnit A0
-pF90Func = parseFree Fortran90 F90.functionParser
-
-pF90pfDefTs :: ByteString -> ProgramFile A0
-pF90pfDefTs = parseFull F90.fortran90Parser
-
-pF90pfNoTs :: ByteString -> ProgramFile A0
-pF90pfNoTs = parseFull (F90.fortran90ParserWithTransforms [])
-
-pF77Stmt :: ByteString -> Statement A0
-pF77Stmt = parseFixed Fortran77 F77.statementParser
-
---pF77Func :: ByteString -> ProgramUnit A0
---pF77Func = parseFixed Fortran77 F77.functionParser
-
-pF77pfDefTs :: ByteString -> ProgramFile A0
-pF77pfDefTs = parseFull F77.fortran77Parser
-
-pF77pfNoTs :: ByteString -> ProgramFile A0
-pF77pfNoTs = parseFull (F77.fortran77ParserWithTransforms [])
-
-pF66Stmt :: ByteString -> Statement A0
-pF66Stmt = parseFixed Fortran66 F66.statementParser
-
---pF66Func :: ByteString -> Statement A0
---pF66Func = parseFixed Fortran66 F66.functionParser
-
-pF66pfDefTs :: ByteString -> ProgramFile A0
-pF66pfDefTs = parseFull F66.fortran66Parser
-
-pF66pfNoTs :: ByteString -> ProgramFile A0
-pF66pfNoTs = parseFull (F66.fortran66ParserWithTransforms [])
-
--- polymorphic on lexer input+token in order to work with fixed & free form
-parseFull
-    :: (ByteString -> String -> ParseResult alexin token (ProgramFile A0))
-    -> ByteString -> ProgramFile A0
-parseFull parser bs =
-    case parser bs "<unknown>" of
-      ParseOk a _   -> a
-      ParseFailed _ -> error "error"
-
-parseFree :: FortranVersion
-          -> Parse LexFree.AlexInput LexFree.Token a -> ByteString -> a
-parseFree ver parser src = evalParse parser parserState
-  where parserState = LexFree.initParseState src ver "<unknown>"
-
-parseFixed :: FortranVersion
-           -> Parse LexFixed.AlexInput LexFixed.Token a -> ByteString -> a
-parseFixed ver parser src = evalParse parser parserState
-  where parserState = LexFixed.initParseState src ver "<unknown>"
+pF90Func = undefined
+pF90pfDefTs = undefined
+pF90pfNoTs = undefined
+pF77Stmt = undefined
+pF77pfDefTs = undefined
+pF77pfNoTs = undefined
+pF66Stmt = undefined
 
 --------------------------------------------------------------------------------
 
@@ -165,4 +117,4 @@ programListing :: [String] -> ByteString
 programListing = strToByteString . intercalate "\n"
 
 strToByteString :: String -> ByteString
-strToByteString = TSE.encodeUtf8 . Text.pack
+strToByteString = Text.encodeUtf8 . Text.pack
---
+++ bench/issue-190.f
@@ -0,0 +1,4 @@
+      subroutine main
+      integer*1 x, int1, a1, a2, a3, a4, a5, a6, a7, a8, a9
+      x = INT(int1, 2)
+      end
