13c126e82d5794a75253e2c766fdac9100d426f7
Raoul Hidalgo Charman <rhidalgochar@bloomberg.net>
2022-08-17 14:47:27 +0100

    Make sure inlined includes aren't reversed
    Also added some tests so this is caught in future.

--- test/Language/Fortran/Parser/Fixed/Fortran77/IncludeSpec.hs
+++ test/Language/Fortran/Parser/Fixed/Fortran77/IncludeSpec.hs
@@ -32,20 +32,25 @@ spec =
           -- it should return the span at the inclusion
           foo = inc </> "foo.f"
           st2Span = makeSrcR (6,7,1, foo) (14,15,1,foo)
-          declSpan = makeSrcR (6,7,1,foo) (14,15,1,foo)
-          typeSpan = makeSrcR (6,7,1,foo) (12,13,1,foo)
-          blockSpan = makeSrcR (14,15,1,foo) (14,15,1,foo)
-          varGen' str =  ExpValue () blockSpan $ ValVariable str
+          st3Span = makeSrcR (22,7,2, foo) (30,15,2,foo)
+          -- declSpan = makeSrcR (6,7,1,foo) (14,15,1,foo)
+          ty1Span = makeSrcR (6,7,1,foo) (12,13,1,foo)
+          ty2Span = makeSrcR (22,7,2,foo) (28,13,2,foo)
+          var1Span = makeSrcR (14,15,1,foo) (14,15,1,foo)
+          var2Span = makeSrcR (30,15,2,foo) (30,15,2,foo)
+          varGen' ss str =  ExpValue () ss $ ValVariable str
 
           pu = PUMain () puSpan (Just name) blocks Nothing
-          blocks = [bl1]
-          decl = Declarator () blockSpan (varGen' "a") ScalarDecl Nothing Nothing
-          typeSpec = TypeSpec () typeSpan TypeInteger Nothing
-          st2 = StDeclaration () st2Span typeSpec Nothing (AList () blockSpan [decl])
-          bl1 = BlStatement () st1Span Nothing st1
-          st1 = StInclude () st1Span ex (Just [bl2])
+          blocks = [bl st1Span st1]
+          decl var = Declarator () (getSpan var) var ScalarDecl Nothing Nothing
+          typeSpec tySpan = TypeSpec () tySpan TypeInteger Nothing
+          st ss tySs var = StDeclaration () ss (typeSpec tySs) Nothing (AList () (getSpan var) [decl var])
+          bl ss = BlStatement () ss Nothing
+          st1 = StInclude () st1Span ex (Just 
+            [ bl st2Span . st st2Span ty1Span $ varGen' var1Span "a"
+            , bl st3Span . st st3Span ty2Span $ varGen' var2Span "b"
+            ])
           ex = ExpValue () expSpan (ValString "foo.f")
-          bl2 = BlStatement () declSpan Nothing st2
     it "includes some files and expands them" $ do
       let inc = "." </> "test-data" </> "f77-include"
       pfParsed <- iParser [inc] source
