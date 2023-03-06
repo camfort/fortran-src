6bfb8560a15172e6bef38f7799851d6b6fd2abc8
Ben Orchard <thefirstmuffinman@gmail.com>
2023-03-06 17:26:56 +0000

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,24 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    #flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, ... }: {
         haskellProjects.default = {
-          #haskellPackages = pkgs.haskell.packages.ghc925;
-          packages = {
-            fortran-src.root = ./.;
-          };
+          # packages.example.root = ./.;  # This value is detected based on .cabal files
+          # overrides = self: super: { };
+          # devShell = {
+          #  enable = true;  # Enabled by default
+          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+          #  hlsCheck.enable = true;
+          # };
         };
+        # haskell-flake doesn't set the default package, but you can do it here.
         packages.default = self'.packages.fortran-src;
       };
     };
