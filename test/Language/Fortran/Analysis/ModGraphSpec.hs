4157d22ccf1a637662264829cf1a819ce32c675b
Dominic Orchard <dom.orchard@gmail.com>
2024-09-03 14:46:32 +0100

    more robust test

--- test/Language/Fortran/Analysis/ModGraphSpec.hs
+++ test/Language/Fortran/Analysis/ModGraphSpec.hs
@@ -21,6 +21,10 @@ testDependencyList = do
     paths' <- expandDirs ["test-data" </> "module"]
     mg <- genModGraph (Just Fortran90) ["."] Nothing paths'
     let list = modGraphToList mg
-    let files = ["leaf.f90", "mid1.f90", "mid2.f90", "top.f90"]
-    let filesWithPaths = map (("test-data" </> "module") </>) files
-    list `shouldBe` filesWithPaths
+    -- we should have two possible orderings
+    let files1 = ["leaf.f90", "mid1.f90", "mid2.f90", "top.f90"]
+    let filesWithPaths1 = map (("test-data" </> "module") </>) files1
+    -- or in a different order
+    let files2 = ["leaf.f90", "mid2.f90", "mid1.f90", "top.f90"]
+    let filesWithPaths2 = map (("test-data" </> "module") </>) files2
+    shouldSatisfy list (\x -> x == filesWithPaths1 || x == filesWithPaths2)
