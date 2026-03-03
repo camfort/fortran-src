85cc3b8cb1a27892dbbff43675e31dae4e1542ba
Matthew Danish <mrd45@cam.ac.uk>
2026-03-03 17:14:34 +0000

    Add xxhash-ffi to GHC 9.2 and 9.4 Nix configurations

--- haskell-flake-ghc94.nix
+++ haskell-flake-ghc94.nix
@@ -12,7 +12,12 @@ pkgs: {
     #singletons.source = "3.0.1";
     th-desugar.source = "1.14";
     th-abstraction.source = "0.4.5.0";
+    xxhash-ffi.source = "0.3";
   };
+
+  otherOverlays = [
+    (self: super: { libxxhash = pkgs.xxHash; })
+  ];
 
   # (note this is actually unused/we have to duplicate because it doesn't get
   # packed into basePackages or any key we can use... but nice to document here)
